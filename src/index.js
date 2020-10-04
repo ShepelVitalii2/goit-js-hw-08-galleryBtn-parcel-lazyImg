cbb3f98ca3c36e483c92a4e763216715d4281ca2
ShepelVitalii <63495011+ShepelVitalii@users.noreply.github.com>
2020-10-04 19:14:47 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
 // import lazySizes from "/lazysizes";
-import variables from "./variables";
 
 import createImageCardMockup from "./js/createImgCardMockup";
-
-const imageCard = createImageCardMockup(images);
-variables.galleryContainer.insertAdjacentHTML("beforeend", imageCard);
